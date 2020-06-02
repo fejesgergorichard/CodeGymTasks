56f33c5ad9d0d9bef6dfaba3c11ad45ce2fc169f
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2020-06-02 20:21:22 +0200

    FileInputStream - Minimum byte in a file

---
+++ 2.JavaCore/src/com/codegym/task/task18/task1803/Solution.java
@@ -0,0 +1,57 @@
+package com.codegym.task.task18.task1803;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Rarest bytes
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String file = reader.readLine();
+        reader.close();
+
+        FileInputStream stream = new FileInputStream(file);
+
+        // we store each byte's count in a hashmap
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int temp, val, newValue;
+        while (stream.available() > 0) {
+            temp = stream.read();
+            if (!map.containsKey(temp)) {
+                map.put(temp, 1);
+            } else {
+                val = map.get(temp).intValue();
+                newValue = (Integer) val + 1;
+                map.replace(temp, newValue);
+            }
+        }
+        // close the file stream
+        stream.close();
+
+
+        // get the minimum value
+        int maximum = Integer.MIN_VALUE;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() >= maximum) {
+                maximum = entry.getValue();
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (Integer.compare(entry.getValue(), maximum ) == 0) {
+                System.out.print(entry.getKey() + " ");
+            }
+        }
+
+    }
+}
