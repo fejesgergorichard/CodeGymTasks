e686fc9ac19e5f9042760592c2906ce4a8399473
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2020-07-21 00:21:49 +0200

    Picking out numbers from a file

---
+++ 2.JavaCore/src/com/codegym/task/task19/task1908/Solution.java
@@ -0,0 +1,43 @@
+package com.codegym.task.task19.task1908;
+
+/* 
+Picking out numbers
+
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader cons = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new FileReader(cons.readLine()));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(cons.readLine()));
+        cons.close();
+
+        StringBuilder sb = new StringBuilder();
+
+        while (reader.ready()) {
+            String line = reader.readLine();
+            String words[] = line.split("\\s");
+            for (String word : words) {
+                if (isDigit(word)) {
+                    sb.append(word);
+                    sb.append(" ");
+                }
+            }
+        }
+        reader.close();
+        sb.deleteCharAt(sb.length()-1);
+        writer.write(sb.toString());
+        writer.close();
+    }
+
+    public static boolean isDigit(String s) {
+        try {
+            int a = Integer.parseInt(s);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
